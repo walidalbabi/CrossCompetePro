c4439519e13d854dd2059ff34d2740adc2d9bc00
walidalbabi <57439495+walidalbabi@users.noreply.github.com>
2021-05-07 05:49:42 +0300

    Updates
    First Push

---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T: MonoBehaviour
+{
+
+    [SerializeField]
+    private bool dontDestroy = false;
+
+    static T _instance;
+    public static T instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<T>();
+                if(_instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    _instance = singleton.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+  public virtual void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this as T;
+
+            if (dontDestroy)
+            {
+                transform.parent = null;
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
